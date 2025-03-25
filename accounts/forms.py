8feef1d240802580ea9b160a7025ab461da4cca6
ModarYaghi <mfyaghi81@gmail.com>
2025-03-25 04:59:20 +0300

    App accounts: adding login form

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,13 @@ from django import forms
 
 
 class LoginForm(forms.Form):
+    """
+    A form to authenticate users against the database.
+
+    The PasswordInput widget is ued to render the password
+    HTML element. This will include `type="password"` in the
+    HTML so that the browser treats it as a password.
+    """
 
     username = forms.CharField()
     password = forms.CharField(widget=forms.passwordInput)
